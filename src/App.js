c13cd51846ebb40753b5dc6534385eef20a09b6d
swapnil2597 <swapnil.khare25@gmail.com>
2020-05-19 23:41:43 +0530

    configuring react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ return (
+  <div className="App">
+   <Navbar dark color="primary">
+    <div className="container">
+     <NavbarBrand href="/">Swapnil's Restaurant</NavbarBrand>
     </div>
-  );
+   </Navbar>
+   <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+     Edit <code> src / App.js </code> and save to reload.{" "}
+    </p>{" "}
+    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+     Learn React{" "}
+    </a>{" "}
+   </header>{" "}
+  </div>
+ );
 }
 
 export default App;

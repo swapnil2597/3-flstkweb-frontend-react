8c4ddfd5c1f834db84573d5a7ef59e34c8305c7a
swapnil2597 <swapnil.khare25@gmail.com>
2020-05-21 21:12:00 +0530

    my assignment 1 approach before required steps applied

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
+import { DISHES } from "./shared/dishes";
 import Menu from "./components/MenuComponent";
 import "./App.css";
+import { Component } from "react";
 
-function App() {
- return (
-  <div className="">
-   <Navbar dark color="primary">
-    <div className="container">
-     <NavbarBrand href="/">Swapnil's Restaurant</NavbarBrand>
-    </div>
-   </Navbar>
-   <Menu />
-  </div>
- );
+class App extends Component {
+ constructor(props) {
+  super(props);
+  this.state = {
+   dishes: DISHES,
+  };
+ }
+
+ render() {
+  return (
+   <div className="">
+    <Navbar dark color="primary">
+     <div className="container">
+      <NavbarBrand href="/">Swapnil's Restaurant</NavbarBrand>
+     </div>
+    </Navbar>
+    <Menu dishes={this.state.dishes} />
+   </div>
+  );
+ }
 }
 
 export default App;
---
+++ src/components/DishDetailComponent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+
+class DishDetail extends Component {
+ constructor(props) {
+  super(props);
+  this.state = {};
+ }
+}

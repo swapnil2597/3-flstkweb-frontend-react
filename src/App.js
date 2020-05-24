e351a541ffc078e8e9e0c80beda045ce746020a2
swapnil2597 <swapnil.khare25@gmail.com>
2020-05-24 06:53:55 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import React from "react";
 import Main from "./components/MainComponent";
 import "./App.css";
 import { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
  render() {
   return (
-   <div className="App">
-    <Main />
-   </div>
+   <BrowserRouter>
+    <div className="App">
+     <Main />
+    </div>
+   </BrowserRouter>
   );
  }
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,7 +7,7 @@ import "../App.css";
 
 function RenderMenuItem({ dish, onClick }) {
  return (
-  <Card onClick={() => onClick(dish.id)}>
+  <Card>
    <CardImg width="100%" src={dish.image} alt={dish.name} />
    <CardImgOverlay>
     <CardTitle>{dish.name}</CardTitle>

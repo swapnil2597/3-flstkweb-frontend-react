1e1b910bd5676fff09ebe2a003d2ee508cdd5a66
swapnil2597 <swapnil.khare25@gmail.com>
2020-05-24 08:24:56 +0530

    Single Page Applications Part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDish({ dish }) {
  if (dish != null) {
   return (
    <Card>
-    <CardImg top src={dish.image} alt={dish.name} />
+    <CardImg top src={dish.image} alt={dish.name} />{" "}
     <CardBody>
      <CardTitle> {dish.name} </CardTitle> <CardText> {dish.description} </CardText>
-    </CardBody>
+    </CardBody>{" "}
    </Card>
   );
  }
@@ -22,8 +23,7 @@ function RenderComments({ comments }) {
     <div>
      <br />
      <p> {elem.comment} </p>
-     --- <span> {elem.author}, </span>
-     <span> {new Intl.DateTimeFormat("en-US", { year: "numeric", month: "short", day: "2-digit" }).format(new Date(Date.parse(elem.date)))} </span>
+     --- <span> {elem.author}, </span> <span> {new Intl.DateTimeFormat("en-US", { year: "numeric", month: "short", day: "2-digit" }).format(new Date(Date.parse(elem.date)))} </span>{" "}
     </div>
    );
   });
@@ -42,16 +42,27 @@ const DishDetail = (props) => {
   return (
    <div className="container">
     <div className="row">
+     <Breadcrumb>
+      <BreadcrumbItem>
+       <Link to="/menu"> Menu </Link>{" "}
+      </BreadcrumbItem>{" "}
+      <BreadcrumbItem active> {props.dish.name} </BreadcrumbItem>{" "}
+     </Breadcrumb>{" "}
+     <div className="col-12">
+      <h3> {props.dish.name} </h3> <hr />
+     </div>{" "}
+    </div>{" "}
+    <div className="row">
      <div className="col-12 col-md-5 mt-4">
-      <RenderDish dish={props.dish} />
-     </div>
+      <RenderDish dish={props.dish} />{" "}
+     </div>{" "}
      <div className="col-12 col-md-5 mt-4">
-      <RenderComments comments={props.dish.comments} />
-     </div>
-    </div>
+      <RenderComments comments={props.comments} />{" "}
+     </div>{" "}
+    </div>{" "}
    </div>
   );
- } else return <div></div>;
+ } else return <div> </div>;
 };
 
 export default DishDetail;
